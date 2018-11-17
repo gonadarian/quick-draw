7d0d4cd19fffe9e2b5d21b96fe3b153bda570227
Igor Popov <gonadarian@gmail.com>
2018-11-18 00:06:32 +0100

    Removed usage of KMeans. Code-style changes.

--- cnn_matrix_predictor.py
+++ cnn_matrix_predictor.py
@@ -6,35 +6,43 @@ from keras.models import load_model
 from PIL import Image, ImageDraw
 
 
-np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
+analysis = False
 
 
-def load_data(show=False):
+def load_data(show=False, shape_choice=1):
     im = Image.new("L", (28, 28), "black")
-
     draw = ImageDraw.Draw(im)
 
-    # draw.line((5, 5, 5, 22), fill=255)
-    # draw.line((5, 5, 22, 5), fill=255)
-    # draw.line((22, 5, 22, 22), fill=255)
-    # draw.line((5, 22, 22, 22), fill=255)
+    if shape_choice == 0:  # shape: square
+        draw.line((5,  5, 5,  22), fill=255)
+        draw.line((5,  5, 22, 5), fill=255)
+        draw.line((22, 5, 22, 22), fill=255)
+        draw.line((5, 22, 22, 22), fill=255)
 
-    # draw.line((5 , 14, 14, 5 ), fill=255)
-    # draw.line((14, 5 , 23, 14), fill=255)
-    # draw.line((23, 14, 14, 23), fill=255)
-    # draw.line((14, 23, 5 , 14), fill=255)
+    if shape_choice == 1:  # shape: rhomboid
+        draw.line((5,  14, 14, 5), fill=255)
+        draw.line((14, 5,  23, 14), fill=255)
+        draw.line((23, 14, 14, 23), fill=255)
+        draw.line((14, 23, 5,  14), fill=255)
 
-    draw.line((2 , 2 , 25, 6 ), fill=255)
-    draw.line((2 , 8 , 25, 12), fill=255)
-    draw.line((2 , 14, 25, 18), fill=255)
-    draw.line((2 , 20, 25, 24), fill=255)
+    if shape_choice == 2:  # shape: tilted, parallel lines
+        draw.line((2, 2,  25, 6), fill=255)
+        draw.line((2, 8,  25, 12), fill=255)
+        draw.line((2, 14, 25, 18), fill=255)
+        draw.line((2, 20, 25, 24), fill=255)
 
-    sample = np.asarray(im)
+    if shape_choice == 3:  # shape: star
+        draw.line((5,  10, 23, 18), fill=255)
+        draw.line((10, 5,  18, 23), fill=255)
+        draw.line((18, 5,  10, 23), fill=255)
+        draw.line((23, 10, 5,  18), fill=255)
+
+    data = np.asarray(im)
     if show:
         im.show()
 
-    sample = sample.astype('float32') / 255.
-    return sample
+    data = data.astype('float32') / 255.
+    return data
 
 
 def show_clusters(input_image, cluster_images):
@@ -51,28 +59,33 @@ def show_clusters(input_image, cluster_images):
     plt.show()
 
 
-sample = load_data(False)
+np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 
-encoder = load_model('models\lines_encoded\lines_mixed_encoded_v2-091-0.000072.hdf5')
+sample = load_data(show=False)
+assert sample.shape == (28, 28)
+
+image_encoder = load_model('models\lines_encoded\lines_mixed_encoded_v2-091-0.000072.hdf5')
 autoencoder = load_model('models\lines\lines_autoencoder_v2-385-0.0047.hdf5')
-decoder = mdls.gen_decoder_model(autoencoder)
-
-embeddings = utl.get_embeddings(encoder, sample, False)
-utl.show_elbow_curve(embeddings, True)
-
+autoencoder_decoder = mdls.gen_decoder_model(autoencoder)
+embeddings = utl.get_embeddings(image_encoder, sample, False)
 clustering_model = load_model('models\pairs_encoded\model_dense_v1-088-0.001555.hdf5')
 cluster_matrix = utl.calculate_cluster_matrix(clustering_model, embeddings)
-plt.imshow(cluster_matrix)
-plt.show()
-
 clusters = utl.extract_clusters(cluster_matrix)
+
+if analysis:
+    utl.show_elbow_curve(embeddings, True)
+    plt.imshow(cluster_matrix)
+    plt.show()
 
 images = []
 for cluster in clusters:
     if len(cluster) > 2:
         cluster_embeddings = embeddings[list(cluster)]
-        # TODO using KMeans is no longer necessary, just average on cluster_embeddings
-        image = utl.decode_clustered_embeddings(decoder, cluster_embeddings, 1, False)[0]
+        cluster_embedding = np.mean(cluster_embeddings, axis=0)
+        encoding, center = utl.extract_encoding_and_center(cluster_embedding)
+        assert encoding.shape == (14, )
+        assert center.shape == (2, )
+        image = utl.gen_image(autoencoder_decoder, encoding, center, show=False)
         images.append(image)
 
 show_clusters(sample, images)
