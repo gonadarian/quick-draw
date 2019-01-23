900daa4887eecbc325eaae307d4680354a90cf33
Igor Popov <gonadarian@gmail.com>
2019-01-23 01:54:57 +0100

    Improved prediction to use new clustering algorithm.

--- conv_matrix_predictor.py
+++ conv_matrix_predictor.py
@@ -3,39 +3,45 @@ import random as rand
 import libs.models as mdl
 import libs.datasets as ds
 import libs.utilities as utl
+import libs.clustering as clst
 import matplotlib.pyplot as plt
-from libs.concepts import Concept
 from PIL import Image, ImageDraw
+from libs.concepts import Concept
 
 
 dim = 27
 vertices = 4
-cluster_threshold = 3
-adjacency_threshold = -30
 channels_full = 17
 channels = 14
 
-mnist = True
-mnist_category = 3
-mnist_sample = None  # 1359  # 5129  # 2583
-quickdraw_data = True
-quickdraw_sample = None
+cluster_threshold = 3
+adjacency_threshold = -30
+similarity_threshold = 0.8
+
+mnist_data = True
+mnist_sample = None  # 5514/9
+mnist_category = None
+quickdraw_data = False
+quickdraw_sample = None  # 64935  # 99209  # 115331
+quickdraw_category = 'triangle'
+custom_data = False
 custom_sample = 3
+
+prediction = 'all'
 analysis = False
 saving = False
 
 np.set_printoptions(formatter={'float': lambda x: "{0:0.4f}".format(x)})
 
 
-def load_quickdraw_dataset(concept):
-    switcher = {
-        Concept.LINE: 'octagon',  # options: triangle, square, octagon
-        Concept.ELLIPSE: 'circle',  # options: circle
-        Concept.BEZIER: 'moon',  # options: animal_migration, rainbow, moon
-    }
+def load_quickdraw_dataset():
 
-    category = switcher.get(concept)
-    x = ds.load_images_quickdraw(category, dim=27)
+    # good choices of categories for different concepts:
+    # - LINE: triangle, square, octagon
+    # - ELLIPSE: circle
+    # - BEZIER: animal_migration, rainbow, moon
+
+    x = ds.load_images_quickdraw(quickdraw_category, dim=27)
 
     return x
 
@@ -83,16 +89,17 @@ def thinned_sample(thinner_model, sample):
     return thinned.reshape(dim, dim)
 
 
-def main(concept):
+def get_sample():
 
-    if mnist:
-        data_set = ds.load_images_mnist(category=mnist_category, dim=dim)
+    if mnist_data:
+        category = rand.randint(0, 9) if mnist_category is None else mnist_category
+        data_set = ds.load_images_mnist(category=category, dim=dim)
         index = rand.randint(0, len(data_set)) if mnist_sample is None else mnist_sample
         print('using mnist sample', index)
         sample = data_set[index, :, :]
 
     elif quickdraw_data:
-        data_set = load_quickdraw_dataset(concept)
+        data_set = load_quickdraw_dataset()  # concept)
         index = rand.randint(0, len(data_set)) if quickdraw_sample is None else quickdraw_sample
         print('using quick draw sample', index)
         sample = data_set[index, :, :]
@@ -106,58 +113,123 @@ def main(concept):
     sample = thinned_sample(thinner_model, sample)
     assert sample.shape == (dim, dim)
 
-    _, _, decoder_model = concept.model_autoencoder()
-    matrix_encoder_model = concept.model_matrix_encoder()
-    clustering_model = concept.model_clustering()
-    sample_threshold = 0.8
+    return sample
 
-    embeddings = utl.get_embeddings(matrix_encoder_model, sample, dim=dim, threshold=sample_threshold, show=False)
-    print('embeddings:', embeddings.shape)
+
+def predict(concept, sample):
+
+    matrix_encoder_model = concept.get_model_matrix_encoder()
+    embeddings = utl.get_embeddings(matrix_encoder_model, sample, dim=dim, threshold=0.8, show=False)
+    print('[predict] embeddings:', embeddings.shape)
+
+    predict_from_embeddings(concept, sample, embeddings)
+
+
+def predict_from_embeddings(concept, sample, embeddings):
+
+    if len(embeddings) == 0:
+        print('[predict_from_embeddings] WARNING: no embeddings provided for concept', concept)
+        return
+
+    _, _, decoder_model = concept.get_model_autoencoder()
+    clustering_model = concept.get_model_clustering()
+
     cluster_matrix = utl.calculate_cluster_matrix(clustering_model, embeddings)
-    print('cluster_matrix:', cluster_matrix.shape)
-    clusters = utl.extract_clusters(cluster_matrix)
-    print('clusters:', len(clusters), [len(cluster) for cluster in clusters])
+    print('[predict_from_embeddings] cluster_matrix:', cluster_matrix.shape)
+
+    # clusters = utl.extract_clusters_v1(cluster_matrix)
+    _, clusters = clst.cluster(embeddings, concept, similarity_threshold=similarity_threshold, show=False)
+    print('[predict_from_embeddings] clusters:', len(clusters), [len(cluster) for cluster in clusters])
 
     if analysis:
-        utl.show_elbow_curve(embeddings, True)
         plt.imshow(cluster_matrix)
         plt.show()
 
-    images = []
     lines = []
-    for cluster in clusters:
-        if len(cluster) <= cluster_threshold:
-            continue
+    images = []
+    weights = []
+
+    for cluster in filter(lambda it: len(it) > cluster_threshold, clusters):
 
         cluster_embeddings = embeddings[list(cluster)]
         cluster_embedding = np.mean(cluster_embeddings, axis=0)
         encoding, center = utl.extract_encoding_and_center(cluster_embedding)
         assert encoding.shape == (channels, )
         assert center.shape == (2, )
-        image = utl.gen_image(decoder_model, encoding, center, dim=dim, show=False)
-        images.append(image)
-        lines.append(cluster_embedding)
 
-    utl.show_clusters(sample, images, dim=dim)
+        image = utl.gen_image(decoder_model, encoding, center, dim=dim, show=False)
+        lines.append(cluster_embedding)
+        images.append(image)
+        weights.append(len(cluster_embeddings))
+
+    utl.show_clusters(sample, images, weights, dim=dim)
 
     adjacency_matrix = utl.get_adjacency_matrix(images, dim=dim, show=True)
     adjacency_matrix = adjacency_matrix > adjacency_threshold
-    print(adjacency_matrix)
-
     edges = utl.get_graph_edges(adjacency_matrix)
-    utl.draw_graph(edges)
 
-    if saving:
-        print(lines)
-        np.save('generator/data/graph_vertices.npy', np.array(lines))
-        print(edges)
-        np.save('generator/data/graph_edges.npy', np.array(edges))
+    if analysis:
+        print(adjacency_matrix)
+        utl.draw_graph(edges)
+
+
+def predict_single(sample, concept):
+
+    sample_threshold = 0.8
+    matrix_encoder_model = concept.get_model_matrix_encoder()
+    embeddings = utl.get_embeddings(matrix_encoder_model, sample, threshold=sample_threshold)
+    print('[predict_single] embeddings:', embeddings.shape)
+
+    similarities = []
+    decoder_model = concept.get_model_autoencoder()[2]
+
+    for i, embedding in enumerate(embeddings):
+        encoding, center = utl.extract_encoding_and_center(embedding)
+        decoded_image = utl.gen_image(decoder_model, encoding, center, dim=dim, show=False)
+        similarity = utl.calc_similarity(sample, decoded_image, algorithm='gauss')
+        similarities.append(similarity)
+
+    return embeddings, similarities
+
+
+def predict_all(sample):
+
+    line_data = predict_single(sample, Concept.LINE)
+    ellipse_data = predict_single(sample, Concept.ELLIPSE)
+    bezier_data = predict_single(sample, Concept.BEZIER)
+
+    similarities = np.vstack((line_data[1], ellipse_data[1], bezier_data[1]))
+    print('[predict_all] similarities:', similarities.shape)
+
+    choices = np.argmax(similarities, axis=0)
+    print('[predict_all] choices:', choices.shape, '\n', choices)
+
+    lines = line_data[0][choices == 0]
+    ellipses = ellipse_data[0][choices == 1]
+    beziers = bezier_data[0][choices == 2]
+
+    print('[predict_all] lines   :', lines.shape)
+    print('[predict_all] ellipses:', ellipses.shape)
+    print('[predict_all] beziers :', beziers.shape)
+
+    predict_from_embeddings(Concept.LINE, sample, lines)
+    predict_from_embeddings(Concept.ELLIPSE, sample, ellipses)
+    predict_from_embeddings(Concept.BEZIER, sample, beziers)
+
+
+def main():
+    sample = get_sample()
+
+    if prediction == 'all':
+        predict_all(sample)
+
+    else:
+        predict(Concept.LINE, sample)
+        predict(Concept.ELLIPSE, sample)
+        predict(Concept.BEZIER, sample)
+        predict(Concept.STAR, sample)
 
 
 if __name__ == '__main__':
-
-    # main(Concept.LINE)
-    # main(Concept.ELLIPSE)
-    main(Concept.BEZIER)
-
-    print('end')
+    while True:
+        main()
